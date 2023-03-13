a46d8676270e55100c8d6c6168f604b03887d872
Oliver Sosa <oliversosa11@gmail.com>
2023-03-13 01:15:48 -0300

    Prueba de menu

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+function navDrop() {
+    var x = document.getElementById("menuDrop");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
