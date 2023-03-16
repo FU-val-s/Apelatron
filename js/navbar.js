477b2a6b43ef5b278de432fa6b89deae1666a81e
Oliver Sosa <oliversosa11@gmail.com>
2023-03-16 11:55:08 -0300

    Prueba de iframe

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,32 @@
-function navDrop() {
-    var x = document.getElementById("menuDrop");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+// function navDrop() {
+//   var x = document.getElementById("menuDrop");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+function triggerIfMenu() {
+  var togglemenu = true;
+  let x = document.getElementsByClassName("ifmenu").contentWindow.document.getElementsByClassName("side-menu");
+  x.on('click', function () {
+    console.log("entraaaa");
+    $('iframe').animate({
+      height: togglemenu ? 200 : 50
+    }, 250);
+
+    togglemenu = !togglemenu;
+
+
+  })
+}
+var togglemenu = true;
+document.getElementsByClassName("ifmenu").contentWindow.document.getElementsByClassName("side-menu").onclick = function () {
+  console.log("entraaaa");
+  document.getElementsByClassName("ifmenu").animate({
+    height: togglemenu ? 200 : 50
+  }, 250);
+
+  togglemenu = !togglemenu;
+}
