12a5d15b379feeb05efec983c071d2d5f6409764
Oliver Sosa <oliversosa11@gmail.com>
2023-03-20 04:51:03 -0300

    Armado del menu desplegable fullscreen

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,24 @@
+
+//const document = undefined;
+var togglemenu = true;
+
+
+$.getElementsByClassName("ifmenu").contentWindow.document.getElementsByClassName("side-menu").onclick = function () {
+  triggerIfMenu();
+}
+
+
+function triggerIfMenu() {
+  let x = $.getElementsByClassName("ifmenu").contentWindow.document.getElementsByClassName("side-menu");
+  x.on('click', function () {
+    console.log("entraaaa");
+    $('iframe').animate({
+      height: togglemenu ? 200 : 50
+    }, 250);
+    togglemenu = !togglemenu;
+  })
+}
+
 // function navDrop() {
 //   var x = document.getElementById("menuDrop");
 //   if (x.style.display === "block") {
@@ -6,27 +27,3 @@
 //     x.style.display = "block";
 //   }
 // }
-
-function triggerIfMenu() {
-  var togglemenu = true;
-  let x = document.getElementsByClassName("ifmenu").contentWindow.document.getElementsByClassName("side-menu");
-  x.on('click', function () {
-    console.log("entraaaa");
-    $('iframe').animate({
-      height: togglemenu ? 200 : 50
-    }, 250);
-
-    togglemenu = !togglemenu;
-
-
-  })
-}
-var togglemenu = true;
-document.getElementsByClassName("ifmenu").contentWindow.document.getElementsByClassName("side-menu").onclick = function () {
-  console.log("entraaaa");
-  document.getElementsByClassName("ifmenu").animate({
-    height: togglemenu ? 200 : 50
-  }, 250);
-
-  togglemenu = !togglemenu;
-}
